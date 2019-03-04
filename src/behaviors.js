0776553f3af8e34eb2693756d3b9caf01ed283a7
byrantwithyou <byrantwithyou@163.com>
2019-03-04 15:42:21 -0500

    authoring tool is ready

---
+++ src/behaviors.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: 1,
+    name: "",
+    description: "",
+    goodExample: "",
+    badExample: "",
+    //Can be Either "Take Snapshot" or "Answer Quiz"
+    detectionMethod: "",
+    severity: 1,
+    //Can be Either Block, Non-Block with Message Reminder, or Non-Block without Message Reminder
+    dealingChoices: ""
+  },
+  {
+    
+  }
+];
--- src/components/Authoring.vue
+++ src/components/Authoring.vue
@@ -2,16 +2,22 @@
   <v-container>
     <v-layout>
       <v-btn>Authoring</v-btn>
+
     </v-layout>
   </v-container>
 </template>
 
 <script>
+  import behaviors from "../behaviors";
   export default {
     data: () => ({
+      settings: {},
+      behaviors: behaviors,
+      subsections: [],
+      steps: []
     }),
     methods:{
-
+          
     }
   }
 </script>
