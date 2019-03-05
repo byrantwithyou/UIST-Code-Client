7f949c3242fe67ceb8158f84febcb382441f9a50
byrantwithyou <byrantwithyou@163.com>
2019-03-04 19:10:18 -0500

    working on step

--- src/components/step.vue
+++ src/components/step.vue
@@ -2,6 +2,8 @@
   <div>
     <h1>Tutorial</h1>
     <h1>Step Content</h1>
+    <h1>Lots of behaviors</h1>
+
   </div>
 </template>
 
@@ -17,6 +19,27 @@
     created: function() {
 
     },
+    computed: {
+      currentStep: function() {
+        return this.$store.state.project.steps[this.$store.state.student.state];
+      },
+      currentBehaviors: function() {
+        let currentBehaviors = [];
+        for (let currentBehavior of this.currentStep.behaviors) {
+          currentBehaviors.push(this.$store.state.project.behaviors[currentBehavior]);
+        }
+        return currentBehaviors;
+      },
+      currentBlockBehaviors: function() {
+        let currentBlockBehaviors = [];
+        for (let currentBehavior of this.currentBehaviors) {
+          if (currentBehavior.dealingMethod == "Block") {
+            currentBlockBehaviors.push(currentBehavior);
+          }
+        }
+        return currentBlockBehaviors;
+      }
+    }
 
   }
 </script>
