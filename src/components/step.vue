2a13e31e437cefccf0c646c4f0d3995da257a65d
byrantwithyou <byrantwithyou@163.com>
2019-03-09 22:22:45 -0500

    sdf

--- src/components/step.vue
+++ src/components/step.vue
@@ -4,7 +4,47 @@
     <v-layout>
       <v-flex xs8 offset-xs2>
         <v-card>
-          <v-btn @click="test">test</v-btn>
+          <v-card-title>
+            <span class="display-1 font-weight-black font-italic blue-grey--text ma-2">Tutorial</span>
+          </v-card-title>
+          <v-card-media contain height="100">
+            <v-img contain height="100" :src="tutorial"></v-img>
+          </v-card-media>
+          <v-card-text>
+            <span class="title font-weight-regular grey--text font-italic">{{currentSubsectionName}} Subsection</span>
+            <v-card-media height="30"></v-card-media>
+            <span class="header font-weight-light black--text">{{currentStepContent}}</span>
+            <v-card-media height="30"></v-card-media>
+            <div class="font-italic" v-for="(behavior, index) in currentBehaviors" :key="index">
+              Behavior {{index + 1}}: {{behavior.name}}
+              <br>
+              Detection Method: {{behavior.detectionMethod}}
+              <br>
+              Level: {{behavior.level}}
+              <br>
+              Dealing Method: {{behavior.dealingMethod}}
+              <br>
+              Behavior Description: {{behavior.description}}
+              <br>
+              <v-layout>
+                <v-flex xs6>
+                  <v-card-media contain height="50">
+                    <v-img contain height="50" :src="behavior.goodExample"></v-img>
+                  </v-card-media>
+                </v-flex>
+                <v-flex xs6>
+                  <v-card-media contain height="50">
+                    <v-img contain height="50" :src="behavior.badExample"></v-img>
+                  </v-card-media>
+                </v-flex>
+              </v-layout>
+              <v-card-media height="30"></v-card-media>
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="nextStep" :disabled="bcknotAuth != 0" outline color="#E53935">Next Step</v-btn>
+          </v-card-actions>
         </v-card>
       </v-flex>
     </v-layout>
@@ -25,25 +65,41 @@
     
     name: "Step",
     data: () => ({
-      currentStepBlockBehaviors: [],
       reviewResult: "",
       reviewResultImage: "",
       reviewResultComment: "",
       behaviorReviewed: "",
-      isReviewResultExisted: false
+      isReviewResultExisted: false,
+      bcknotAuth: 0
     }),
     methods: {
-      test: function() {
+      nextStep: function() {
         this.$store.commit("project/addStep");
       }
     },
     computed: {
       currentStepNumber: function() {
         return this.$store.state.project.step;
+      },
+      tutorial: function() {
+        return this.$store.state.project.settings["projectTutorial"];
+      },
+      currentSubsectionName: function() {
+        return this.$store.state.project.subsections[this.$store.state.project.currentSubsection - 1].name;
+      },
+      currentStepContent: function() {
+        return this.$store.state.project.currentStepContent;
+      },
+      currentBehaviors: function() {
+        return this.$store.state.project.currentBehaviors;
       }
     },
     created: function() {
-      
+      for (let currentBehavior of this.currentBehaviors) {
+        if (currentBehavior.dealingMethod == "Block") {
+          this.bcknotAuth += 1;
+        }
+      }
     },
     components: {
      // ReviewResult
