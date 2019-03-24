c624d30045f3541f9ae0fbc57933452d6dbb4343
byrantwithyou <byrantwithyou@163.com>
2019-03-23 21:12:02 -0400

    all is done

--- src/components/Authoring.vue
+++ src/components/Authoring.vue
@@ -14,6 +14,28 @@
           <AuthoringSubsections v-if="authoringStep==4"></AuthoringSubsections>
         </v-card>
       </v-flex>
+      <v-flex xs5 offset-xs1 v-if="authoringStep==4">
+        <v-card>
+          <v-card-title>
+            <span class="font-weight-black indigo--text">
+              Take care of these sequence-based styles: 
+            </span>
+          </v-card-title>
+          <v-card-text>
+            <div>
+              <code>Begin by fixing the ICs first. Then start connecting the remaining relevant components to the breadboard sockets which directly reach out to the IC pins.</code>
+            </div>
+            <v-card-media height="50"></v-card-media>
+            <div>
+              <code>Always connect the batteries or power supply to the circuit last, check the whole circuit and then power the circuit</code>
+            </div>
+            <v-card-media height="50"></v-card-media>
+              <div>
+                <code>Build and test in subsections</code>
+              </div>
+          </v-card-text>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
