b27b760f7b8fb4247e632963d9d3f3d94672e6d4
hhcyforever <changyang.he.young@gmail.com>
2019-03-25 10:06:12 +0800

    Chinese version plus

--- src/steps.js
+++ src/steps.js
@@ -8,60 +8,60 @@ let steps = [
   {
     behaviors: [],
     content:
-      "Connect the jumper wire from the power rail on the breadboard to H26 (you'll want to select a place on the power rail for your wire near A27)",
+      "用跳线连接面包板正轨与H26插孔（如图为A27旁边的位置）",
     number: 1
   },
   {
     behaviors: [],
     content:
-      "Place the pushbutton switch into F24, F26, E24 and E26 on the breadboard",
+      "将按钮连接到面包板上的F24、F26、E24和E26",
     number: 2
   },
   {
     behaviors: [],
     content:
-      "Place one leg of a 10k Ohm resistor in G19 and the other leg in G24 on the breadboard",
+      "将一个10k欧姆电阻器的一个脚放在G19上，另一个脚放在G24上",
     number: 5
   },
   {
     behaviors: [],
     content:
-      "Connect the jumper wire from the GND rail (blue line) on the breadboard to H19 (you'll want to select a place on the GND rail for your wire near A17)",
+      "将跳线从面包板上的负轨连接到H19(如图为A17旁边的位置，蓝线连接)",
     number: 6
   },
   {
     behaviors: [],
     content:
-      "Connect the jumper wire from pin 13 on the Arduino to i5 on the breadboard",
+      "用跳线连接Arduino上的pin13与面包板上的I5",
     number: 0
   },
   {
     behaviors: [],
     content:
-      "Place one leg of a 220 Ohm resistor in D5 and the other leg in G5 on the breadboard",
+      "将220欧姆电阻器的一只脚放在D5中，另一只脚放在G5",
     number: 1
   },
   {
     behaviors: [],
     content:
-      "Insert the short leg of the LED in the GND rail (blue line) around A5 and the long leg in B5 on the breadboard",
+      "将LED的短腿插入A5旁的负轨上，长腿插入面包板上的B5中",
     number: 2
   },
   {
     behaviors: [],
     content:
-      "Connect the jumper wire from the GND on the Arduino to the GND rail (blue line) on the breadboard near A1",
+      "用跳线连接Arduino上的GND孔与面包板负轨",
     number: 0
   },
   {
     behaviors: [],
     content:
-      "Connect the jumper wire from 5V on the Arduino to power rail (red line) near A1 on the breadboard",
+      "用跳线连接Arduino上的5V跳线与面包板上正轨",
     number: 0
   },
   {
     behaviors: [],
-    content: "Connect the Arduino Uno to your computer via USB cable",
+    content: "通过USB将Arduino Uno连接到您的计算机",
     number: 0
   }
 ];
